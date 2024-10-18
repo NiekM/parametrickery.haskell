6023fe2532988f010b954b55f9bbb0ed31645036
Mulleners <n.mulleners@uu.nl>
2024-10-18 16:15:01 +0200

    Fix bug in relevance

--- src/Language/Relevance.hs
+++ src/Language/Relevance.hs
@@ -15,10 +15,10 @@ import Language.Coverage
 subs :: Set a -> [Set a]
 subs = Set.toList . Set.powerSet
 
-minimal :: Ord a => [Set a] -> [Set a]
-minimal xs = filter (\x -> not $ any (`Set.isProperSubsetOf` x) xs) xs
+maximal :: Ord a => [Set a] -> [Set a]
+maximal xs = filter (\x -> not $ any (x `Set.isProperSubsetOf`) xs) xs
 
-sufficient :: forall sig m. Has (Reader Context) sig m =>
+sufficient :: Has (Reader Context) sig m =>
   Set Name -> Problem -> m (Either Conflict (Signature, [Rule], Coverage))
 sufficient xs problem = runError do
   let restricted = onArgs (disable xs) problem
@@ -36,6 +36,6 @@ relevance problem = do
   xs <- catMaybes <$> forM sets \set -> do
     s <- sufficient set problem
     return $ (set,) <$> either (const Nothing) return s
-  let ss = minimal (map fst xs)
+  let ss = maximal (map fst xs)
   let ys = map snd . filter ((`elem` ss) . fst) $ xs
   return . Relevance . NonEmpty.fromList $ ys
