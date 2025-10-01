4353322c7e2897d8261c77839028ac3a722c9216
Niek Mulleners <niekmulleners@gmail.com>
2025-10-01 10:31:19 +0200

    Add filter variants

--- src/Tactic/Filter.hs
+++ src/Tactic/Filter.hs
@@ -3,10 +3,12 @@ module Tactic.Filter where
 import Base
 import Control.Effect.Fresh.Named
 import Data.List qualified as List
+import Data.Some
 
 import Language.Expr
 import Language.Problem
 import Language.Type
+import Language.Generics
 
 import Tactic.Core
 import Tactic.Check
@@ -38,6 +40,102 @@ filter name = do
         local (const subproblem) do
           f <- rerealize hole
           let result = Apps (Var "filter") [Lams [x] f, Var name]
+          return result
+      _ -> throwError $ NotApplicable "filter only works on lists"
+
+-- Generalized filter whose predicate has access to a nonempty list.
+filterSome :: Tactic sig m => Name -> m Filling
+filterSome name = do
+  Arg mono terms <- getArg name
+  local (hide [name]) do
+    problem <- ask @Problem
+    case (mono, problem.signature.output) of
+      (Data "List" [t], Data "List" [u]) -> do
+        when (t /= u) $ throwError $ NotApplicable "list types do not match"
+        examples <- forM (zip terms problem.examples) \case
+          (List inputs, Example scope (List outputs)) -> do
+            unless (isFilter inputs outputs) $ throwError $ PropagationError "not a filter"
+            let
+              xs = case inputs of
+                [] -> error "cannot be"
+                (y:ys) -> toExpr $ toSome y ys
+            return $ List.nub inputs <&> \x ->
+              Example (scope ++ [x, xs]) $ Bool $ x `elem` outputs
+          _ -> error "Not actually lists."
+        x <- freshName "x"
+        xs <- freshName "xs"
+        let
+          Signature constraints context _ = problem.signature
+          signature =
+            Signature constraints (context ++ [Named x t, Named xs (Data "Some" [t])]) (Data "Bool" [])
+          subproblem = Problem signature $ concat examples
+        local (const subproblem) do
+          f <- rerealize hole
+          let result = Apps (Var "filterSome") [Lams [x, xs] f, Var name]
+          return result
+      _ -> throwError $ NotApplicable "filter only works on lists"
+
+mapFocus :: ([a] -> a -> [a] -> b) -> [a] -> [b]
+mapFocus f = go []
+  where
+    go _ [] = []
+    go ys (x:xs) = f ys x xs : go (x:ys) xs
+
+-- Generalized filter whose predicate has access to the rest of the list
+filterRest :: Tactic sig m => Name -> m Filling
+filterRest name = do
+  Arg mono terms <- getArg name
+  local (hide [name]) do
+    problem <- ask @Problem
+    case (mono, problem.signature.output) of
+      (Data "List" [t], Data "List" [u]) -> do
+        when (t /= u) $ throwError $ NotApplicable "list types do not match"
+        examples <- forM (zip terms problem.examples) \case
+          (List inputs, Example scope (List outputs)) -> do
+            -- TODO: check if this is really correct, since this is more general than `isFilter` allows, I think.
+            unless (isFilter inputs outputs) $ throwError $ PropagationError "not a filter"
+            return $ List.nub inputs & mapFocus \as x bs ->
+              Example (scope ++ [x, List (as ++ bs)]) $ Bool $ x `elem` outputs
+          _ -> error "Not actually lists."
+        x <- freshName "x"
+        xs <- freshName "xs"
+        let
+          Signature constraints context _ = problem.signature
+          signature =
+            Signature constraints (context ++ [Named x t, Named xs (Data "List" [t])]) (Data "Bool" [])
+          subproblem = Problem signature $ concat examples
+        local (const subproblem) do
+          f <- rerealize hole
+          let result = Apps (Var "filterRest") [Lams [x, xs] f, Var name]
+          return result
+      _ -> throwError $ NotApplicable "filter only works on lists"
+
+-- filter whose predicate has access to the whole input list
+filterArg :: Tactic sig m => Name -> m Filling
+filterArg name = do
+  Arg mono terms <- getArg name
+  local (hide [name]) do
+    problem <- ask @Problem
+    case (mono, problem.signature.output) of
+      (Data "List" [t], Data "List" [u]) -> do
+        when (t /= u) $ throwError $ NotApplicable "list types do not match"
+        examples <- forM (zip terms problem.examples) \case
+          (List inputs, Example scope (List outputs)) -> do
+            -- TODO: check if this is really correct, since this is more general than `isFilter` allows, I think.
+            unless (isFilter inputs outputs) $ throwError $ PropagationError "not a filter"
+            return $ List.nub inputs <&> \x ->
+              Example (scope ++ [x, List inputs]) $ Bool $ x `elem` outputs
+          _ -> error "Not actually lists."
+        x <- freshName "x"
+        xs <- freshName "xs"
+        let
+          Signature constraints context _ = problem.signature
+          signature =
+            Signature constraints (context ++ [Named x t, Named xs (Data "List" [t])]) (Data "Bool" [])
+          subproblem = Problem signature $ concat examples
+        local (const subproblem) do
+          f <- rerealize hole
+          let result = Apps (Var "filterArg") [Lams [x, xs] f, Var name]
           return result
       _ -> throwError $ NotApplicable "filter only works on lists"
 
